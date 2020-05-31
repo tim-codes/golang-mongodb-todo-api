479a37dfd3f950ebde3631479ee06c2e2c5b1e7d
Tim O'Connell <tim@splyt.com>
2020-06-01 00:11:25 +0800

    basic mongo client setup

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	fmt.Println("Starting mongo todo app...")
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27011,localhost:27012,localhost:27013/playments-playground?replicaSet=rs0")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
